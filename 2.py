9525aa4974b1aebcd9a770eed0ec50c6a62bba19
Nickglumov <nekitglum@gmail.com>
2025-01-12 20:05:25 +0200

    lesson 2

---
+++ 2.py
@@ -0,0 +1,9 @@
+letter = """\
+From: glum0vnick@yandex.ru
+To: glum0vnick@yandex.ru
+Subject: Приглашение
+Content-Type: text/plain; charset="UTF-8";
+
+letter = letter.encode("UTF-8")
+
+print(letter)
